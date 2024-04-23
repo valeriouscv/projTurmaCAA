3827e08f1d631bc13c7962197f05a530aa19d7be
Valério Santos <130845898+valeriouscv@users.noreply.github.com>
2024-04-23 16:50:13 -0100

    inclusão do Scanner no projeto

--- Principal.java
+++ Principal.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class Principal{
     public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
         System.out.println("Projeto Turma");
+
     }
 }
