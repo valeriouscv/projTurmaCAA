1029fdf1c4ee89ff0893cb76cefa8008cb7d89cd
Valério Santos <130845898+valeriouscv@users.noreply.github.com>
2024-04-23 16:55:11 -0100

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class Principal{
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        System.out.println("Projeto Turma");
+        System.out.println("Projeto Turma - CAA");
+        
 
     }
 }
